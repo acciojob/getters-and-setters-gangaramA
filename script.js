a04fa3241885d85dbc5b96551b5c40485daa9be7
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-12-27 19:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-//complete this code
-class Person {}
+// Define the Person class
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    // Getter for name
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    // Setter for age
+    set age(age) {
+        this._age = age;
+    }
+
+    // Getter for age to verify the updated age
+    get age() {
+        return this._age;
+    }
+}
+
+// Define the Student class inheriting from Person
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+// Define the Teacher class inheriting from Person
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
